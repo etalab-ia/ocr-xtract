4aef49f8c1b6721de878ee8d4dcdcc9f21e2ac6d
Pavel Soriano <pavel.soriano@data.gouv.fr>
2021-06-30 23:47:38 +0200

    adding path->Document transformer + AnnotationDatasetCreator

--- src/salaire/expe_baseline.py
+++ src/salaire/expe_baseline.py
@@ -10,7 +10,7 @@ Runs a baseline info extraction system. The procedure is as follows:
 4. Train a simple classification algorithm to predict the above mentioned classes
 
 """
-
+import glob
 from pathlib import Path
 from pprint import pprint
 
@@ -18,27 +18,32 @@ from doctr.models import ocr_predictor
 from doctr.documents import DocumentFile
 
 from doctr_utils import get_doctr_info, extract_words, WindowTransformer
+from src.salaire.annotation_utils import DoctrTransformer, AnnotationDatasetCreator
 
 IMG_PATH = "/data/dossierfacil/salary/notvalidated_png/68945ca2-1758-48d0-b2bd-4ebb056fa752.pdf-1.png"
 IMG_PATH = "data/CNI_caro2.jpg"
 IMG_PATH = "data/476922b7-0bdf-414c-a7ef-6c1a0c3618c9.jpg"
+IMG_FOLDER_PATH = Path("./data/CNI")
+TRAINING_DATA_PATH = Path("./data/training_data/cni_annotation.csv")
+def main():
+    list_img_paths = list(sorted([f for f in IMG_FOLDER_PATH.iterdir() if f.suffix in [".jpg", ".png"]]))
+    doctr_documents = DoctrTransformer().transform(list_img_paths)
+    dataset_creator = AnnotationDatasetCreator(output_path=TRAINING_DATA_PATH, raw_documents=list_img_paths)
+    dataset_creator.transform(doctr_documents)
+    exit(0)
 
-def create_windows(horizontal: int = 3, vertical: int = 3):
-    pass
 
-
-def main(image_path: Path):
-    doct_output = get_doctr_info(image_path) for image_path in list
+    # doct_output = get_doctr_info(image_path) for image_path in list
     # doct_output.pages[0].blocks[0].lines[0].words
     windower = WindowTransformer(line_eps=0.01)
-    windower.fit([doct_output])
-    windower._transform([doct_output])
+    windower.fit(doctr_documents)
+    windower._transform(doctr_documents)
     # windower.get_sourrounding_words(id_box=10)
     pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main(IMG_PATH)
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
