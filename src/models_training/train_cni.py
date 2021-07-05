36e82409b50fbb93a56e17728585dca8522be2f7
Rob192 <reynaud.robin@gmail.com>
2021-07-05 13:22:39 +0200

    feat: create script for training model on CNI

--- src/models_training/train_cni.py
+++ src/models_training/train_cni.py
@@ -6,15 +6,15 @@ from sklearn.preprocessing import LabelBinarizer
 
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import f1_score, precision_score, recall_score, classification_report
 from sklearn.tree import DecisionTreeClassifier
 
 from src.salaire.doctr_utils import WindowTransformerList
 import numpy as np
 
 
-data_train = pd.read_csv("./data/CNI_recto/cni_annotation_recto_train.csv", sep='\t')
-data_test = pd.read_csv("./data/CNI_recto/cni_annotation_recto_test.csv", sep=';')
+data_train = pd.read_csv("./data/CNI_recto_aligned/cni_annotation_recto_train.csv", sep=';')
+data_test = pd.read_csv("./data/CNI_recto_aligned/cni_annotation_recto_test.csv", sep=';')
 columns = data_train.columns.to_list()
 columns.remove('label')
 X_train, y_train = data_train[columns], data_train["label"]
@@ -25,32 +25,59 @@ y_train = lb.fit_transform(y_train)
 y_test = lb.transform(y_test)
 
 wt = WindowTransformerList()
-X_train = wt.fit_transform(X_train)
-X_test = wt.transform(X_test)
+X_train = np.transpose(wt.fit_transform(X_train))
+X_test = np.transpose(wt.transform(X_test))
 
-classifier = OneVsRestClassifier(LinearSVC(random_state=0))
-classifier.fit(np.transpose(X_train), y_train)
-y_pred = classifier.predict(np.transpose(X_test))
+X_train_angle = X_train[:,:int(X_train.shape[1] / 2)] #take first half that corresponds to the angles
+X_test_angle = X_test[:,:int(X_train.shape[1] / 2)]
 
-print(f"f1: {f1_score(y_test, y_pred, average='weighted')}")
-print(f"precision_score: {precision_score(y_test, y_pred, average='weighted')}")
-print(f"recall_score: {recall_score(y_test, y_pred, average='weighted')}")
+X_train_distance = X_train[:,int(X_train.shape[1] / 2):] #take first half that corresponds to the angles
+X_test_distance = X_test[:,int(X_train.shape[1] / 2):]
 
-
-
-classifier = OneVsRestClassifier(DecisionTreeClassifier(random_state=0))
-classifier.fit(np.transpose(X_train), y_train)
-y_pred = classifier.predict(np.transpose(X_test))
-
-print(f"f1: {f1_score(y_test, y_pred, average='weighted')}")
-print(f"precision_score: {precision_score(y_test, y_pred, average='weighted')}")
-print(f"recall_score: {recall_score(y_test, y_pred, average='weighted')}")
+X_train_is_left = np.where(np.logical_or(np.pi -0.1 < X_train_angle, X_train_angle < -np.pi + 0.1), 1.0,0.0)
+X_test_is_left = np.where(np.logical_or(np.pi -0.1 < X_test_angle, X_test_angle < -np.pi + 0.1), 1.0,0.0)
 
 
-classifier = OneVsRestClassifier(RandomForestClassifier(random_state=0))
-classifier.fit(np.transpose(X_train), y_train)
-y_pred = classifier.predict(np.transpose(X_test))
+# X_train = np.concatenate([X_train_distance, X_train_is_left], axis=1)
+# X_test = np.concatenate([X_test_distance, X_test_is_left], axis=1)
 
-print(f"f1: {f1_score(y_test, y_pred, average='weighted')}")
-print(f"precision_score: {precision_score(y_test, y_pred, average='weighted')}")
-print(f"recall_score: {recall_score(y_test, y_pred, average='weighted')}")
+
+
+# classifier = OneVsRestClassifier(LinearSVC(random_state=0))
+# classifier.fit(X_train, y_train)
+# y_pred = classifier.predict(X_test)
+#
+# print(f"f1: {f1_score(y_test, y_pred, average='macro')}")
+# print(f"precision_score: {precision_score(y_test, y_pred, average='macro')}")
+# print(f"recall_score: {recall_score(y_test, y_pred, average='macro')}")
+
+
+# classifier = OneVsRestClassifier(DecisionTreeClassifier(random_state=0))
+# classifier.fit(X_train, y_train)
+# y_pred = classifier.predict(X_test)
+#
+# print(classification_report(y_test, y_pred))
+# print(f"f1: {f1_score(y_test, y_pred, average='macro')}")
+# print(f"precision_score: {precision_score(y_test, y_pred, average='macro')}")
+# print(f"recall_score: {recall_score(y_test, y_pred, average='macro')}")
+#
+classifier = (DecisionTreeClassifier(random_state=0))
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+print(classification_report(y_test, y_pred))
+#
+# print(f"f1: {f1_score(y_test, y_pred, average='macro')}")
+# print(f"precision_score: {precision_score(y_test, y_pred, average='macro')}")
+# print(f"recall_score: {recall_score(y_test, y_pred, average='macro')}")
+#
+#
+# classifier = OneVsRestClassifier(RandomForestClassifier(random_state=0))
+# classifier.fit(X_train, y_train)
+# y_pred = classifier.predict(X_test)
+#
+# print(f"f1: {f1_score(y_test, y_pred, average='macro')}")
+# print(f"precision_score: {precision_score(y_test, y_pred, average='macro')}")
+# print(f"recall_score: {recall_score(y_test, y_pred, average='macro')}")
+#
+# y_pred = lb.inverse_transform(y_pred)
+# np.savetxt("./data/CNI_recto/y_pred.csv", y_pred, delimiter=",", fmt='%s')
