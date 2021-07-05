072fa9efd2221d3a8ba3554f3ab3d89f3422181c
Rob192 <reynaud.robin@gmail.com>
2021-07-05 13:22:17 +0200

    refactor: move script expe_baseline in data

---
+++ src/data/prepare_data_for_annotation.py
@@ -0,0 +1,29 @@
+"""
+Runs a baseline info extraction system. The procedure is as follows:
+1. Extract boxes with doctr
+2. For each word, gather the following features:
+    a. min_x, min_y, max_x, max_y coordinates
+    b. spacy embedding
+    c. is_in_name_list
+    d. is_in_prenom_list
+3. Save them in a csv
+"""
+
+from pathlib import Path
+
+
+from src.salaire.annotation_utils import DoctrTransformer, AnnotationDatasetCreator
+
+IMG_FOLDER_PATH = Path("./data/CNI_recto_aligned/test")
+TRAINING_DATA_PATH = Path("./data/CNI_recto_aligned/cni_annotation_recto_test.csv")
+
+
+def main():
+    list_img_paths = list(sorted([f for f in IMG_FOLDER_PATH.iterdir() if f.suffix in [".jpg", ".png"]]))
+    doctr_documents = DoctrTransformer().transform(list_img_paths)
+    dataset_creator = AnnotationDatasetCreator(output_path=TRAINING_DATA_PATH, raw_documents=list_img_paths)
+    dataset_creator.transform(doctr_documents)
+
+
+if __name__ == '__main__':
+    main()
