eaa32d09bf6aa7a90555f2bf0dceb140a90514f1
KimMontalibet <kim.montalibet@gmail.com>
2021-07-28 16:59:43 +0200

    fix error in AnnotationJsonCreator class

--- src/data/script_prepare_CNI_annotation.py
+++ src/data/script_prepare_CNI_annotation.py
@@ -2,8 +2,10 @@ from src.salaire.annotation_utils import DoctrTransformer, AnnotationJsonCreator
 from pathlib import Path
 import os
 
+
 img_folder_path = "data/CNI_recto_aligned_linux/train"
 output_path = "data/CNI_recto_aligned_linux/cni_annotation_train_docker.json"
+
 
 def main():
     list_img_path = [Path(os.path.join(img_folder_path, x)) for x in os.listdir(img_folder_path)]
