c3c8ab702bae1bab7678409f70cee6ce73b33fd5
Rob192 <reynaud.robin@gmail.com>
2021-09-10 10:41:06 +0200

    Feat: Update CNI model

--- src/models_training/train_cni_pipeline.py
+++ src/models_training/train_cni_pipeline.py
@@ -17,8 +17,8 @@ from src.salaire.doctr_utils import WindowTransformerList, BoxPositionGetter, Co
 import numpy as np
 
 
-data_train = pd.read_csv("./data/salary/small_annotated_train.csv", sep='\t')
-data_test = pd.read_csv("./data/salary/small_annotated_test.csv", sep='\t')
+data_train = pd.read_csv("./data/cni_recto_csv_for_training/train_annotated.csv", sep='\t')
+data_test = pd.read_csv("./data/cni_recto_csv_for_training/test_annotated.csv", sep='\t')
 columns = data_train.columns.to_list()
 columns.remove('label')
 X_train, y_train = data_train[columns], data_train["label"]
@@ -27,7 +27,6 @@ X_test, y_test = data_test[columns], data_test["label"]
 
 pipe = Pipeline([
     ('feature_union', FeatureUnion([('window_transformer', WindowTransformerList(searched_words=['Nom:','Prénom(s):','Né(e)'])),
-                                    ('bag_of_words', BagOfWordInLine(searched_words=['Nom:', 'Prénom(s):', 'Né(e)'])),
                                     ("position", BoxPositionGetter()),
                                     ('is_digit', ContainsDigit()),
                                     ('is_nom', IsNom()),
@@ -48,7 +47,7 @@ print(X_test)
 print(classification_report(y_test, y_pred))
 
 from pickle import dump
-with open('./model/model_test_h', 'wb') as f1:
+with open('./model/CNI_model', 'wb') as f1:
     dump(pipe, f1)
 # with open('./model/CNI_label', 'wb') as f2:
 #     dump(lb, f2)
