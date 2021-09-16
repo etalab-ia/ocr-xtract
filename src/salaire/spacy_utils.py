ef9d91185cccdd6a0117e142e06ed01b468a8c68
Rob192 <reynaud.robin@gmail.com>
2021-09-16 15:40:16 +0200

    Trials on spacy_utils.py

--- src/salaire/spacy_utils.py
+++ src/salaire/spacy_utils.py
@@ -2,7 +2,14 @@ import spacy
 import numpy as np
 nlp = spacy.load('fr_core_news_sm')
 
+text="Michel est allée au 20 rue des martyrs Madrid le 24 Juin 2020"
+words=text.split() # this is to simulate the words that would come from Doctr
+doc = spacy.tokens.doc.Doc(
+    nlp.vocab, words=words)
+for name, proc in nlp.pipeline:
+    doc = proc(doc)
 
-def return_word_vector(text: str) -> np.array:
-    doc = nlp(text)
-    return doc
+for d in doc.ents:
+    print(d.label_)
+
+
