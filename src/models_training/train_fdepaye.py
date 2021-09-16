90b9eed5fb59e4f64714343c3839a37fb0849eb9
Rob192 <reynaud.robin@gmail.com>
2021-09-16 15:38:25 +0200

    feat: calculate the optimal number of lines that JenksNaturalBreaks will use

--- src/models_training/train_fdepaye.py
+++ src/models_training/train_fdepaye.py
@@ -16,6 +16,8 @@ from src.salaire.doctr_utils import WindowTransformerList, BoxPositionGetter, Co
     BagOfWordInLine
 import numpy as np
 
+from tpot import TPOTClassifier
+
 
 if __name__ == "__main__":
     data_train = pd.read_csv("./data/salary_for_training/train_annotated.csv", sep='\t')
@@ -30,27 +32,40 @@ if __name__ == "__main__":
     X_train, y_train = data_train[columns], data_train["label"]
     X_test, y_test = data_test[columns], data_test["label"]
 
-    pipe = Pipeline([
-        ('feature_union', FeatureUnion([('window_transformer', WindowTransformerList(searched_words=['salaire','net','impots','periode','revenu'])),
-                                        ('bag_of_words', BagOfWordInLine(searched_words=['salaire','net','impots','periode','revenu'])),
-                                        # ('is_date', IsDate()),
-                                        ("position", BoxPositionGetter()),
-                                        ('is_digit', ContainsDigit()),
-                                        ('is_nom', IsNom()),
-                                        ('is_prenom', IsPrenom()),
-                                        ])),
-        ('decision_tree', GradientBoostingClassifier())
-    ])
+    pipe_feature = FeatureUnion([('window_transformer', WindowTransformerList(searched_words=['salaire','net','impots','periode','revenu'])),
+                                ('bag_of_words', BagOfWordInLine(searched_words=['salaire','net','impots','periode','revenu'])),
+                                ('is_date', IsDate()),
+                                ("position", BoxPositionGetter()),
+                                ('is_digit', ContainsDigit()),
+                                ('is_nom', IsNom()),
+                                ('is_prenom', IsPrenom()),
+                                ])
 
 
-    pipe.fit(X_train, y_train)
-    y_pred = pipe.predict(X_test)
+
+    X_train = pipe_feature.fit_transform(X_train)
+    df = pd.DataFrame(X_train, columns=pipe_feature.get_feature_names())
+    X_test = pipe_feature.transform(X_test)
+
+    tpot = TPOTClassifier(generations=5, population_size=20,  verbosity=2, random_state=42, n_jobs=-1)
+    tpot.fit(X_train, y_train)
+
+    y_pred = tpot.predict(X_test)
+    X_test['label']=y_pred
 
     print(classification_report(y_test, y_pred))
 
+    tpot.export('./model/tpot_exported_pipeline.py')
+
+    """
+    y_pred = pipe.predict(X_test)
+    X_test['label']=y_pred
+    print(X_test)
+    print(classification_report(y_test, y_pred))
+    
     from pickle import dump
     with open('./model/model_test_h', 'wb') as f1:
         dump(pipe, f1)
-
+    """
 
 
