c7d06440d2d0bacd097d72e18e0c7abf6b9b65ec
Rob192 <reynaud.robin@gmail.com>
2021-10-27 11:06:48 +0200

    Add dvc eval.py in dvc pipeline

--- src/models_training/utils.py
+++ src/models_training/utils.py
@@ -2,6 +2,16 @@ from tqdm import tqdm
 import pandas as pd
 
 def select_candidate(candidate_name, candidate_feature, features, X, y=None):
+    """
+    This function is used to select the words that can be candidates for a certain field.
+
+    :param candidate_name: the name of the candidate. This must be the name used during annotation
+    :param candidate_feature: this is the name of the feature used for the candidate selection
+    :param features: the name of the features
+    :param X: the features data
+    :param y: the labelled data (optionnal)
+    :return: the features and the labelled data with only the candidates selected
+    """
     df = pd.DataFrame(X, columns=features)
     if y is not None:
         df['label'] = y.values
