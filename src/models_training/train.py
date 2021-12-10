7c106603019b3aada77adef24a5e375625cda73f
Rob192 <reynaud.robin@gmail.com>
2021-12-10 13:58:22 +0100

    train model without optimization

--- src/models_training/train.py
+++ src/models_training/train.py
@@ -12,7 +12,7 @@ from sklearn.pipeline import Pipeline
 from src.models_training.utils import select_candidate, tqdm_skopt
 
 
-def train_optimized_model(candidate, data, n_cv=3, n_points=1, n_iter=100, n_estimators=100):
+def prepare_data(candidate, data):
     candidate_name = candidate['training_field']
     candidate_feature = candidate['candidate']
     pipe_feature = data['pipe_feature']
@@ -21,38 +21,15 @@ def train_optimized_model(candidate, data, n_cv=3, n_points=1, n_iter=100, n_est
     features = pipe_feature.get_feature_names()
     X_train, y_train = select_candidate(candidate_name, candidate_feature, features, X_train, y_train)
 
-    pipe_feature_post = Pipeline([
-        ('power_transformer', PowerTransformer()),
-    ]) # TODO : do we really need to do that here ?
+    return X_train, y_train
 
-    X_train = pipe_feature_post.fit_transform(X_train)
-
-    pipe = BayesSearchCV(
-        GradientBoostingClassifier(n_estimators=n_estimators, verbose=0),
-        {
-            'max_leaf_nodes': (10, 60),
-            'learning_rate': (0.05, 0.2, 'uniform'),
-            'max_depth': (3, 100),  # integer valued parameter
-        },
-        n_points=n_points,
-        n_jobs=n_cv*n_points,
-        n_iter=n_iter,
-        cv=n_cv,
-        verbose=0,
-        scoring='f1_macro',
-    )
-    pipe.fit(X_train, y_train, callback=[tqdm_skopt(total=int(n_iter/(n_cv*n_points)), desc=candidate_name)])
-
-    print("val. score: %s" % pipe.best_score_)
-
-    return pipe_feature_post, pipe
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         sys.stderr.write("Arguments error. Usage:\n")
-        sys.stderr.write("\tpython train.py features-dir-path scheme-path model-dir-path\n")
+        sys.stderr.write("\tpython train.py features-dir-path scheme-path model-dir-path param_name\n")
         sys.exit(1)
 
     train_input = os.path.join(sys.argv[1], "data.pickle")
@@ -60,32 +37,68 @@ if __name__ == "__main__":
 
     os.makedirs(os.path.join(sys.argv[3]), exist_ok=True)
 
+
+
     # load data and scheme
     with open(train_input, 'rb') as f1:
         data = load(f1)
     with open(scheme_file, 'rb') as f_s:
         scheme = json.load(f_s)
 
-    params = yaml.safe_load(open("params.yaml"))["train"]
+    params = yaml.safe_load(open("params.yaml"))[sys.argv[4]]
 
+    optimize = params['optimize']
     n_iter = params['n_iter']
     n_estimators = params['n_estimators']
 
+    if not optimize:
+        learning_rate = params['learning_rate']
+        max_depth = params['max_depth']
+        max_leaf_nodes = params['max_leaf_nodes']
 
     n_scheme = len(scheme.keys())
     n_cpu = os.cpu_count()
     n_cv = 3  # cross validation for optimization
     n_points = max(n_cpu // n_cv, 1)
     for candidate, candidate_name in zip(scheme.values(), scheme.keys()):
-        model_output = os.path.join(sys.argv[3], candidate_name + '-' + "optimized_GB")
+
+        X_train, y_train = prepare_data(candidate, data)
+        pipe_feature_post = Pipeline([
+            ('power_transformer', PowerTransformer()),
+        ])  # TODO : do we really need to do that here ?
+
+        X_train = pipe_feature_post.fit_transform(X_train)
         model = {}
-        model['pipe_feature_post'], \
-        model['model'] = train_optimized_model(candidate,
-                                               data,
-                                               n_cv=n_cv,
-                                               n_points=n_points,
-                                               n_iter=n_iter,
-                                               n_estimators=n_estimators)
+        model['pipe_feature_post'] = pipe_feature_post
+
+        if optimize:
+            model_output = os.path.join(sys.argv[3], candidate_name + '-' + "optimized_GB")
+            pipe = BayesSearchCV(
+                GradientBoostingClassifier(n_estimators=n_estimators, verbose=0),
+                {
+                    'max_leaf_nodes': (10, 60),
+                    'learning_rate': (0.05, 0.2, 'uniform'),
+                    'max_depth': (3, 100),  # integer valued parameter
+                },
+                n_points=n_points,
+                n_jobs=n_cv * n_points,
+                n_iter=n_iter,
+                cv=n_cv,
+                verbose=0,
+                scoring='f1_macro',
+            )
+            pipe.fit(X_train, y_train,
+                     callback=[tqdm_skopt(total=int(n_iter / (n_cv * n_points)), desc=candidate_name)])
+
+            print("val. score: %s" % pipe.best_score_)
+            model['model'] = pipe
+
+        else:
+            model_output = os.path.join(sys.argv[3], candidate_name + '-' + "GB")
+            gbc = GradientBoostingClassifier(learning_rate=learning_rate, n_estimators=n_estimators,
+                                             max_depth=max_depth, max_leaf_nodes=max_leaf_nodes)
+            gbc.fit(X_train, y_train)
+            model['model'] = gbc
 
         with open(model_output, 'wb') as f1:
             dump(model, f1)
