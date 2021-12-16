b792995fac345a97b5718d46e9b6fb9c8a329358
Rob192 <reynaud.robin@gmail.com>
2021-12-16 15:22:37 +0100

    move results from mlflow to json

--- src/models_training/eval.py
+++ src/models_training/eval.py
@@ -4,44 +4,22 @@ from pickle import load
 import json
 
 from sklearn.metrics import f1_score
-from tqdm import tqdm
-
-import mlflow
-from mlflow.tracking import MlflowClient
-from dotenv import load_dotenv
 
 from src.models_training.utils import select_candidate
 
-load_dotenv()
-
-def prepare_mlflow_server():
-    try:
-        tracking_uri = os.getenv("MLFLOW_TRACKING_SERVER_URI")
-        if tracking_uri:
-            mlflow.set_tracking_uri(tracking_uri)
-            tqdm.write(f"MLflow tracking to server {tracking_uri}")
-            pass
-        else:
-            tqdm.write(f"MLflow tracking to local mlruns folder")
-    except Exception as e:
-        tqdm.write(f"Not using remote tracking servers. Error {e}")
-        tqdm.write(f"MLflow tracking to local mlruns folder")
-
-
 
 if __name__ == "__main__":
-    prepare_mlflow_server()
-    client = MlflowClient()
-
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         sys.stderr.write("Arguments error. Usage:\n")
-        sys.stderr.write("\tpython eval.py features-dir-path scheme-path model-dir-path\n")
+        sys.stderr.write("\tpython eval.py features-dir-path scheme-path model-dir-path results-dir-path\n")
         sys.exit(1)
 
     features_folder = os.path.join(sys.argv[1], "data.pickle")
     scheme_name = sys.argv[2].split('/')[1]
     scheme_path = sys.argv[2]
-    model_folder = os.path.join(sys.argv[3])
+    model_folder = sys.argv[3]
+    results_folder = sys.argv[4]
+    os.makedirs(results_folder, exist_ok=True)
 
     # load data and scheme
     with open(features_folder, 'rb') as f1:
@@ -55,15 +33,15 @@ if __name__ == "__main__":
 
     list_model = os.listdir(model_folder)
 
+    results = {}
+
     for candidate_name in scheme.keys():
         print(candidate_name)
         candidate_feature = scheme[candidate_name]['candidate']
 
         list_model_candidate = [m for m in list_model if candidate_name == m.split('-')[0]]
 
-        expe = scheme_name + '_' + candidate_name
-        mlflow.set_experiment(experiment_name=expe)
-        experiment = client.get_experiment_by_name(expe)
+        results[candidate_name] = {}
 
         for model_name in list_model_candidate:
             with open(os.path.join(model_folder, model_name), 'rb') as f:
@@ -79,7 +57,7 @@ if __name__ == "__main__":
             f1 = f1_score(y, y_pred)
             print(f1)
 
-            run = client.create_run(experiment.experiment_id)
-            client.log_param(run.info.run_id, 'model', model_name)
-            client.log_metric(run.info.run_id, 'f1', f1)
-            client.log_artifact(run.info.run_id, 'dvc.lock')
+            results[candidate_name][model_name] = f1
+
+    with open(os.path.join(results_folder, 'results.json'), 'w') as f:
+        json.dump(results, f)
