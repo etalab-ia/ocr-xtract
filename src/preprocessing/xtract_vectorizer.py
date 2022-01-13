74d1f3f09e5d7df0a099ced9f796e687328d5b55
Rob192 <reynaud.robin@gmail.com>
2022-01-13 15:41:46 +0100

    improve mapping of values for WindowTransformerList

--- src/preprocessing/xtract_vectorizer.py
+++ src/preprocessing/xtract_vectorizer.py
@@ -135,8 +135,8 @@ class WindowTransformerList(XtractVectorizer):
 
     def get_relative_positions(self, vocab_i, list_plain_words_in_page, df):
         # TODO : the calculation of the distances between the words of the vocab is done twice, we should avoid that
-        array_angles = np.zeros(len(list_plain_words_in_page))  # false value for angle
-        array_distances = np.ones(len(list_plain_words_in_page)) * 1  # max distance
+        array_angles = np.ones(len(list_plain_words_in_page))  # false value for angle
+        array_distances = np.ones(len(list_plain_words_in_page)) # max distance
         if vocab_i in list_plain_words_in_page:
             wi_list = [i for i, x in enumerate(list_plain_words_in_page) if x == vocab_i]
             for wi in wi_list:
@@ -145,7 +145,7 @@ class WindowTransformerList(XtractVectorizer):
                     x_j, y_j = df.iloc[j]['min_x'], df.iloc[j]['min_y']
                     distance = cosine((x_i, y_i), (x_j, y_j))
                     if distance < array_distances[j]:  # in case there are several identical duplicate of vocab i, take the closest
-                        array_angles[j] = np.arctan2((y_j - y_i), (x_j - x_i))
+                        array_angles[j] = np.arctan2((y_j - y_i), (x_j - x_i)) / np.pi
                         array_distances[j] = distance
         return array_angles, array_distances
 
@@ -164,7 +164,7 @@ class WindowTransformerList(XtractVectorizer):
                 res.append(
                     self.get_relative_positions(vocab_i=voc, list_plain_words_in_page=list_plain_words_in_page, df=df))
             array_angle = np.array([r[0] for r in res]).T
-            array_distance = np.array([r[0] for r in res]).T
+            array_distance = np.array([r[1] for r in res]).T
             return array_angle, array_distance
 
 
