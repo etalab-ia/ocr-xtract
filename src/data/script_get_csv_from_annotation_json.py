fa878526a46fda5e6078ee9ee8c2ce4d37b0d482
KimMontalibet <kim.montalibet@gmail.com>
2022-01-31 17:34:32 +0100

    add new rotated dataset

--- src/data/script_get_csv_from_annotation_json.py
+++ src/data/script_get_csv_from_annotation_json.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from src.data.annotation_utils import LabelStudioConvertor
 
 annotation_json = Path("data/quittances/annotation/sample1.json")
-output_path = Path("data/quittances/annotation/sample_1.csv")
+annotation_json = Path("data/bulletins/preannotation/salary_preannotation_sample_first_rotated.json")
+output_path = Path("data/bulletins/annotation/sample_1_rotated.csv")
 
-df_annotation = LabelStudioConvertor(annotation_json, output_path).transform()
+df_annotation = LabelStudioConvertor(annotation_json, output_path, False).transform()
 df_annotation.to_csv(output_path, index=False, sep= "\t")
